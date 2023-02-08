f3e7a7fddb87c0d0d7cb5682ced7c45e8e9c9698
IngilizAdam <hasantuna0070@gmail.com>
2023-02-08 21:36:51 +0300

    GUN 4

--- GUN 3/MetotCagirma.java
+++ GUN 3/MetotCagirma.java
@@ -3,5 +3,6 @@ public class MetotCagirma {
         Metot.print("Hello World");
         double ortalama = Metot.ortalamaHesapla(10, 20);
         Metot.print(ortalama);
+        double ortalama2 = Metot.ortalamaHesapla(5, 2);
     }
 }
